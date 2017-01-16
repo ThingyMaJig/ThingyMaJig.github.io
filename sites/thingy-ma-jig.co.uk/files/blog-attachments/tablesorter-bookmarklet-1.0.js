89d35f58b1f93dccc84d46497c31dbb1a0a5c4ab
Nicholas Thompson <nick@fullfatthings.com>
2017-01-16 10:02:31 +0000

    Adding Drupal site files

---
+++ sites/thingy-ma-jig.co.uk/files/blog-attachments/tablesorter-bookmarklet-1.0.js
@@ -0,0 +1,9 @@
+﻿window.bookmarklet = function(opts){fullFunc(opts)};
+window.bookmarklet({
+  css : ['http://tablesorter.com/themes/blue/style.css'],
+  js  : ['http://autobahn.tablesorter.com/jquery.tablesorter.min.js'],    
+  ready : function() {
+    $("table").tablesorter().addClass("tablesorter"); 
+  }
+});
+function fullFunc(a){function d(b){if(b.length===0){a.ready();return false}$.getScript(b[0],function(){d(b.slice(1))})}function e(b){$.each(b,function(c,f){$("<link>").attr({href:f,rel:"stylesheet"}).appendTo("head")})}a.jqpath=a.jqpath||"http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js";(function(b){var c=document.createElement("script");c.type="text/javascript";c.src=b;c.onload=function(){e(a.css);d(a.js)};document.body.appendChild(c)})(a.jqpath)};
